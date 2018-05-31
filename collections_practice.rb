96a024691107eeb14ba5866bc11343fcd9845123
Paul Bomba <paul@paulbomba.com>
2018-05-31 01:08:28 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -80,6 +80,6 @@ end
 def organize_schools(schools)
 end
 
- #   {[{:first_name => "blake"}, {:first_name => "ashley"}]}
+ #   [{:first_name => "blake"}, {:first_name => "ashley"}]
 
   #  [{"blake" => { :awesomeness => 10, :height => "74", :last_name => "johnson"}, "ashley" => { :awesomeness => 9, :height => 60, :last_name => "dubs"}}]
