c6078ccfe0b4cea3ea821e2a3c0ed53aaaff3437
Paul Bomba <paul@paulbomba.com>
2018-05-31 01:11:37 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -84,3 +84,6 @@ end
  #   [{:first_name => "blake"}, {:first_name => "ashley"}]
 
   #  [{"blake" => { :awesomeness => 10, :height => "74", :last_name => "johnson"}, "ashley" => { :awesomeness => 9, :height => 60, :last_name => "dubs"}}]
+  
+  
+  
